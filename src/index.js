61712daf94632f05f3102f07be21a91ce228d1e1
martagreen <portnykhmaria@gmail.com>
2021-03-10 18:55:25 +0400

    feat: add avg function

--- src/index.js
+++ src/index.js
@@ -18,5 +18,8 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array == undefined) return 0;
+  sum_num = 0;
+  for (let i = 0; i < array.length; i++) sum_num += array[i]
+  return (sum_num/array.length)
 }
