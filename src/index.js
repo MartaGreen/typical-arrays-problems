f642a562c1756fb9f52ae35120c3551ce228efb2
martagreen <portnykhmaria@gmail.com>
2021-03-10 19:20:50 +0400

    fix: check if param is empty

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
 exports.min = function min (array) {
-  if (!array || array == undefined) return 0;
-  min_num = array[i];
-  for (let i = 0; i < array.length; i++) {
+  if (typeof array == "undefined" || !array.length ) return 0;
+  min_num = array[0];
+  for (let i = 1; i < array.length; i++) {
     if (min_num > array[i]) min_num = array[i];
   };
   return min_num;
 }
 
 exports.max = function max (array) {
-  if (!array || array == undefined) return 0;
+  if (typeof array == "undefined" || !array.length ) return 0;
   max_num = 0;
   for (let i = 0; i < array.length; i++) {
     if (max_num < array[i]) max_num = array[i];
@@ -18,7 +18,7 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  if (!array || array == undefined) return 0;
+  if (typeof array == "undefined" || !array.length ) return 0;
   sum_num = 0;
   for (let i = 0; i < array.length; i++) sum_num += array[i]
   return (sum_num/array.length)
